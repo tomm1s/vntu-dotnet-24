f235f50d398242bfa39c4c1baca8e7c33305ffaf
Yevhenii Sentiurin <frytsreaction@gmail.com>
2024-08-19 21:57:54 +0300

    classes for prorgam

---
+++ toom1s.TaskPlanner.Domain.Models/toom1s.TaskPlanner.Domain.Models/Enums/Priority.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toom1s.TaskPlanner.Domain.Models.Enums
+{
+    public enum Priority
+    {
+        None,
+        Low,
+        Medium,
+        High,
+        Urgent  
+    }
+}
